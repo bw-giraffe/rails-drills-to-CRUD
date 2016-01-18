dd6349a055babd8476e100355dff3bacd74825e6
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-01-17 17:05:37 -0800

    created article and user models in addition to sessions controller

--- trial1/config/routes.rb
+++ trial1/config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
+  get 'articles/show'
+
+  get 'articles/create'
+
+  get 'articles/new'
+
+  get 'articles/edit'
+
+  get 'articles/update'
+
+  get 'articles/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
