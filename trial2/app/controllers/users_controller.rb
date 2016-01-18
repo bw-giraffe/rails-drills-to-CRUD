61d2418d5ae24d37804bd9756d9e21e4b589119c
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-01-18 15:41:35 -0800

    created models, migrated, created associations, added user, model, sessions controllers

---
+++ trial2/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
