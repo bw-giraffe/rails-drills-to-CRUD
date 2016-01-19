be6f0fb3267f9c20f0597849d81bdcbcb0f5454a
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-01-18 20:40:43 -0800

    stuck on article edit

--- trial2/app/controllers/articles_controller.rb
+++ trial2/app/controllers/articles_controller.rb
@@ -1,22 +1,49 @@
 class ArticlesController < ApplicationController
+  before_filter :logged_in?, except: [:index, :show]
+  
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find_by_id(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
-  end
-
-  def update
+    @article = current_user.articles.new(article_params)
+    if @article.save
+      redirect_to "/articles/#{@article.id}"
+    else
+      redirect_to new_article_path
+    end
   end
 
   def edit
+    @article = Article.find_by_id(params[:id])
+  end
+
+  def update
+    p 'YOU HAVE ENTERED UPDATEEEEEEEEE'
+    p article_params
+    @article = @article.update(article_params)
+    if @article.update
+      redirect_to "/articles/#{@article.id}"
+    else
+      redirect_to "/articles"
+    end 
   end
 
   def destroy
   end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
--- trial2/config/routes.rb
+++ trial2/config/routes.rb
@@ -1,24 +1,40 @@
 Rails.application.routes.draw do
   root to: "users#index"  
-  # get 'sessions/new'
+ 
   get "/users", to: "users#index", as: "users"
 
   # get 'sessions/create'
   get "/users/new", to: "users#new", as: "new_user"
 
   post "/users", to: "users#create"
-  # get 'articles/index'
+
   get "/users/:id", to: "users#show", as: "user"
 
-  # get 'articles/show'
+
   get "/login", to: "sessions#new"
 
-  # get 'articles/new'
   post "/sessions", to: "sessions#create"
 
-  # get 'articles/create'
   get "/logout", to: "sessions#destroy"
 
+  get "/articles", to: "articles#index"
+
+  get "/articles/new", to: "articles#new", as: "new_article"
+  
+  get "/articles/:id", to: "articles#show"
+
+  post "/articles/create", to: "articles#create"
+
+  get "/articles/:id/edit", to: "articles#edit"
+
+  patch "/articles/:id/update", to: "articles#update"
+
+  # get 'articles/index'
+ # get 'sessions/new'
+  # get 'articles/create'
+  # get 'sessions/create'
+  # get 'articles/show'
+  # get 'articles/new'
 
   # get 'articles/update'
 
