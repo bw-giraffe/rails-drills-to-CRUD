dc91da65452d9bd57030c275bd587b828e7dcc73
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-01-18 22:29:04 -0800

    seeded data

--- trial2/db/seeds.rb
+++ trial2/db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+	5.times do 
+		article1 = {}
+		article2 = {}
+		2.times do
+			article1 = Article.create({
+				:title => FFaker::Company.bs,
+				:content => FFaker::HipsterIpsum.phrase
+				})
+			article2 = Article.create({
+				:title => FFaker::Company.bs,
+				:content => FFaker::HipsterIpsum.phrase
+				})
+		end
+
+		user = User.create({
+			:last_name => FFaker::Name.last_name,
+			:first_name => FFaker::Name.first_name,
+			:email => FFaker::Internet.email,
+			:password => FFaker::Internet.domain_word
+			})
+
+	 	user.articles << article1
+	 	user.articles << article2 
+	end
+
